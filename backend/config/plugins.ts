5fa0047cbb3124d78377feeb7773d1c58fa53f51
Dardan Demiri <dardantheman@gmail.com>
2026-01-08 20:43:23 -0500

    Update S3 upload configuration to set public-read ACL for uploaded files

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -30,9 +30,10 @@ export default ({ env }) => ({
             secretAccessKey: env('S3_SECRET_ACCESS_KEY'),
           },
           forcePathStyle: env.bool('S3_FORCE_PATH_STYLE', true),
-          params: {
-            Bucket: env('S3_BUCKET'),
-          },
+        },
+        params: {
+          Bucket: env('S3_BUCKET'),
+          ACL: 'public-read',
         },
       },
     },
